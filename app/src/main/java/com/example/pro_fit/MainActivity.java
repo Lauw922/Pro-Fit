40ccdfd8ea85db713f4ef3f6907fda89963fc9db
Lauréline <laureline.drieu@gmail.com>
2023-11-30 15:18:52 +0100

    2nd commit maj- correction bug

--- app/src/main/java/com/example/pro_fit/MainActivity.java
+++ app/src/main/java/com/example/pro_fit/MainActivity.java
@@ -2,15 +2,38 @@ package com.example.pro_fit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn_go_register;
+
+    public void initUi(){
+        btn_go_register = findViewById(R.id.btn_goRegister);
+
+    }
+
+    public void setBtn_go_register(){
+        btn_go_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("TAG", "onClick: Cliquer");
+                    startActivity(new Intent(MainActivity.this, Register.class));
+
+            }
+        });
+    }
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initUi();
+        setBtn_go_register();
     }
 }
--- app/src/main/java/com/example/pro_fit/Register.java
+++ app/src/main/java/com/example/pro_fit/Register.java
@@ -1,42 +1,63 @@
 package com.example.pro_fit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Register extends AppCompatActivity {
 
-    TextInputEditText editFirstName, editLastName, editMail, editPassword;
-    Button buttonReg, buttonLog;
+    Button btn_login, btn_register;
+    EditText et_first_name, et_last_name, et_mail, et_password;
+    FirebaseAuth mAuth;
 
-    @SuppressLint("WrongViewCast")
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
 
-        editFirstName = findViewById(R.id.et_firstname);
-        editLastName = findViewById(R.id.et_lastname);
-        editMail = findViewById(R.id.et_mail);
-        editPassword = findViewById(R.id.et_password);
-        buttonReg = findViewById(R.id.btn_register);
-        buttonLog = findViewById(R.id.btn_login);
 
-        buttonReg.setOnClickListener(new View.OnClickListener() {
+    public void initUi(){
+        mAuth = FirebaseAuth.getInstance();
+        btn_login = findViewById(R.id.btn_login);
+        btn_register = findViewById(R.id.btn_register);
+        et_first_name = findViewById(R.id.et_firstname);
+        et_last_name = findViewById(R.id.et_lastname);
+        et_mail = findViewById(R.id.et_mail);
+        et_password = findViewById(R.id.et_password);
+    }
+
+    public void setBtn_login(){
+        btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email,password, fname, lname;
-                email = String.valueOf(editMail.getText());
-                password = String.valueOf(editPassword.getText());
-                fname = String.valueOf(editFirstName.getText());
-                lname = String.valueOf(editLastName.getText());
+                startActivity(new Intent(Register.this, Login.class));
+
+            }
+        });
+    }
+
+
+    public void setBtn_register(){
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email, password, fname, lname;
+                email = String.valueOf(et_mail.getText());
+                password = String.valueOf(et_password.getText());
+                fname = String.valueOf(et_first_name.getText());
+                lname = String.valueOf(et_last_name.getText());
 
                 if (TextUtils.isEmpty(email)){
                     Toast.makeText(Register.this, "Entrez un email", Toast.LENGTH_SHORT).show();
@@ -58,9 +79,34 @@ public class Register extends AppCompatActivity {
                     return;
                 }
 
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+
+                                    Toast.makeText(Register.this, "Account created",
+                                            Toast.LENGTH_SHORT).show();
+
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(Register.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
             }
         });
-
-
     }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        initUi();
+        setBtn_login();
+        setBtn_register();
+    }
+
 }
